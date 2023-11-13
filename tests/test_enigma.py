3c3be629cc5d99912041192b9beccfb73055d623
Stanislav Chubar <stas-chubar@yandex.ru>
2023-11-13 17:15:14 +0700

    fix test_enigma.py: fixed test algorithm

--- tests/test_enigma.py
+++ tests/test_enigma.py
@@ -5,12 +5,12 @@ from source.enigma import Enigma  # Подставьте правильный п
 class TestEnigma(unittest.TestCase):
     def setUp(self):
         # Подготовка данных для тестов
-        self.rotor_a = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-                        'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-        self.rotor_b = ['G', 'Q', 'W', 'F', 'M', 'D', 'A', 'Z', 'T', 'X', 'Y', 'S', 'E', 'U', 'V', 'P', 'B', 'R', 'L',
-                        'I', 'N', 'O', 'C', 'J', 'K', 'H']
-        self.rotor_c = ['R', 'Y', 'O', 'T', 'Z', 'P', 'K', 'S', 'Q', 'W', 'G', 'V', 'X', 'U', 'C', 'F', 'I', 'A', 'H',
-                        'D', 'N', 'L', 'J', 'M', 'B', 'E']
+        self.rotor_a = ['E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S',
+                        'P', 'A', 'I', 'B', 'R', 'C', 'J']
+        self.rotor_b = ['A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z',
+                        'N', 'P', 'Y', 'F', 'V', 'O', 'E']
+        self.rotor_c = ['B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G',
+                        'A', 'K', 'M', 'U', 'S', 'Q', 'O']
 
         self.enigma = Enigma(self.rotor_a, self.rotor_b, self.rotor_c)
 
@@ -27,15 +27,17 @@ class TestEnigma(unittest.TestCase):
 
     def test_get_rotor_letter(self):
         # Проверка получения буквы из ротора
+        self.enigma.change_rotors_pos(1,1,1)
         letter = self.enigma.get_rotor_letter(self.rotor_a, 3)
-        self.assertEqual(letter, 'C')
+        self.assertEqual(letter, 'M')
 
     def test_convert(self):
         # Проверка кодирования строки
         encoded_string = self.enigma.convert('HELLO')
-        self.assertEqual(encoded_string, 'HJSVD')
+        self.assertEqual(encoded_string, 'CKBQL')
 
         # Проверка декодирования строки
+        self.enigma.change_rotors_pos(1, 1, 1)
         decoded_string = self.enigma.convert(encoded_string, reverse=True)
         self.assertEqual(decoded_string, 'HELLO')
 
