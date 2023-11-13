92de4db485f8e89df07d693a0ce2a0fd182993e7
Stanislav <snorccrafter@gmail.com>
2023-11-13 13:30:04 +0700

    upd reading rotors config

--- source/configuration/enigma_config.py
+++ source/configuration/enigma_config.py
@@ -2,13 +2,12 @@ from pathlib import Path
 
 
 def read_rotor(rotor_number: int) -> list:
-    if 1 > rotor_number > 4:
+    if not (1 <= rotor_number <= 4):
         return []
     _rotor_file_name = f"rotor{rotor_number}.txt"
     _rotor_path = Path(Path.cwd(), "configuration", _rotor_file_name)
     _rotor_config = []
     with open(_rotor_path, "r") as rotor:
-        for line in rotor:
-            _rotor_config.append(str(line[0]))
+        _rotor_config = rotor.read().strip()
 
     return _rotor_config
