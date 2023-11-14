1ff1fef3998075331ff6c468a45e73aa6072dd36
Stanislav <snorccrafter@gmail.com>
2023-11-14 12:16:28 +0700

    code from __main__.py migrated to console.py

---
+++ enigmaemu/console.py
@@ -0,0 +1,50 @@
+from configuration import enigma_config
+from enigma import Enigma
+
+# import re
+
+if __name__ == "__main__":
+    rotors = []
+    for rotor_number in range(1, 4, 1):
+        rotors.append(enigma_config.read_rotor(rotor_number))
+
+    enigma = Enigma(rotors[0], rotors[1], rotors[2])
+    enigma.state()
+
+    while True:
+        print("[1] - ЗАКОДИРОВАТЬ\n"
+              "[2] - РАСКОДИРОВАТЬ\n"
+              "[3] - СМЕНИТЬ ПОЗИЦИЮ РОТОРА\n"
+              "[4] - ТЕКУЩИЙ СТАТУС ЭНИГМЫ\n"
+              "[НЕ цифра] - ВЫХОД")
+        try:
+            answer = int(input(">>>"))
+        except Exception:
+            raise SystemExit
+
+        match answer:
+            case 1:
+                message = input("ТЕКСТ НА АНГЛИЙСКОМ БЕЗ ПРОБЕЛОВ >>> ").upper()
+                print(enigma.convert(message, reverse=False))
+
+            case 2:
+                message = input("ТЕКСТ НА АНГЛИЙСКОМ БЕЗ ПРОБЕЛОВ >>> ").upper()
+                decoded_text = enigma.convert(message, reverse=True)
+                # words = re.findall('[A-Z]+[a-z]*', decoded_text)
+                # result = ' '.join(words) # TODO : make words separating
+                print(decoded_text)
+
+            case 3:
+                rotors_pos = enigma.get_rotors_pos()
+                print(f"ТЕКУЩИЕ ПОЗИЦИИ РОТОРОВ [A:{rotors_pos[0]}, B:{rotors_pos[1]}, C:{rotors_pos[2]}]")
+                a_pos = int(input("ПОЗИЦИЯ РОТОРА A >>> "))
+                b_pos = int(input("ПОЗИЦИЯ РОТОРА B >>> "))
+                c_pos = int(input("ПОЗИЦИЯ РОТОРА C >>> "))
+                if 1 <= (a_pos or b_pos or c_pos) <= 26:
+                    enigma.change_rotors_pos(a_pos, b_pos, c_pos)
+            case 4:
+                enigma.state()
+
+        rotors_pos = enigma.get_rotors_pos()
+        print(f"ПОЗИЦИИ РОТОРОВ ПОСЛЕ ОПЕРАЦИИ [A:{rotors_pos[0]}, B:{rotors_pos[1]}, C:{rotors_pos[2]}]")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
