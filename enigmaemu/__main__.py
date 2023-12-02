fadae861f93f6dc10ff86e34cb213303adb3f608
Stanislav <snorccrafter@gmail.com>
2023-12-02 15:32:24 +0700

    future:start basic cli-side developing

---
+++ enigmaemu/__main__.py
@@ -0,0 +1,10 @@
+# Previous code migrated to console.py
+# __main__.py now is handler for cli
+
+import click
+
+from configuration import enigma_config
+from enigma import Enigma
+
+if __name__ == "__main__":
+    pass
